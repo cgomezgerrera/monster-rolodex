c2d164ae71fdfaa6ac4b4b7ce2f83842a660c974
cgomezgerrera <carogherrera.9411@gmail.com>
2020-04-19 00:15:52 -0400

    Updates

---
+++ precache-manifest.515693835a9366b226f78192fe76e3ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9905a0c83ec2bb0691ded23ffb309fbe",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ad2d5df03c0cffb95e00",
+    "url": "/monster-rolodex/static/css/main.5982fd47.chunk.css"
+  },
+  {
+    "revision": "9d84f5b306774dfc9f1a",
+    "url": "/monster-rolodex/static/js/2.91e906d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.91e906d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad2d5df03c0cffb95e00",
+    "url": "/monster-rolodex/static/js/main.bec8e36d.chunk.js"
+  },
+  {
+    "revision": "238059c1b43ed123953f",
+    "url": "/monster-rolodex/static/js/runtime-main.14143c00.js"
+  }
+]);
